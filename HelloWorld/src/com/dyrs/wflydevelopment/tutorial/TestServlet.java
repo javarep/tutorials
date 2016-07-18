62d3f1715fccd33f1104bfacbc6685bd0014e925
doracl <dorwcl@gmail.com>
2016-07-18 18:09:06 +0800

    helloworld

---
+++ HelloWorld/src/com/dyrs/wflydevelopment/tutorial/TestServlet.java
@@ -0,0 +1,55 @@
+package com.dyrs.wflydevelopment.tutorial;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String CONTENT_TYPE = "text/html;charset=UTF-8";
+	private static final String MESSAGE = "<!DOCTYPE html><html>" + "<head><title>Hello!</title></head>"
+			+ "<body>Hello World Wildfly</body>" + "</html>";
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TestServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at:
+		// ").append(request.getContextPath());
+		response.setContentType(CONTENT_TYPE);
+		try (PrintWriter out = response.getWriter()) {
+			out.println(MESSAGE);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
